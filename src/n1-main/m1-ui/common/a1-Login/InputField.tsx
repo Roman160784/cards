9a25319ca747c9945661d3f24a849c4b7aef8d84
lenocheklisichka <47819860+lenocheklisichka@users.noreply.github.com>
2022-04-02 19:43:26 +0300

    rendered jsx component

---
+++ src/n1-main/m1-ui/common/a1-Login/InputField.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const InputField = () => {
+    return <div>
+        <input aria-label={"email"} type={"email"}/>
+        <input aria-label={"password"} type={"password"}/>
+    </div>
+}
--- src/n1-main/m1-ui/common/a1-Login/Login.tsx
+++ src/n1-main/m1-ui/common/a1-Login/Login.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import SuperInputText from "../c1-SuperInputText/SuperInputText";
+import {InputField} from "./InputField";
+import {Button} from "./Button";
 
 export const Login = () => {
-    return(
-        <div>LOGIN</div>
+    return (
+        <div>
+            <form style={{width: "200px"}}>
+                <h1>LOGIN</h1>
+                <InputField/>
+                <Button/>
+            </form>
+        </div>
     )
 }
