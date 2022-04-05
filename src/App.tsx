79738243a598045b02b22777825c8310e3b56175
Roman160784 <matveyenkaraman@gmail.com>
2022-04-05 12:22:27 +0300

    add Preloader component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Main } from './n1-main/m1-ui/routes/a0-Main/Main';
-
+import { Preloader } from './n1-main/m1-ui/common/c5-Preloader/Preloader';
 
 
 function App() {
   return (
     <div className="App">
+      <Preloader/>
       <Main/>
     </div>
   );
