573736683fadbb33316e527e6ed51ce8bf6baf0f
Roman160784 <matveyenkaraman@gmail.com>
2022-04-06 18:30:38 +0300

    Updated imports

--- src/n1-main/m1-ui/routes/a0-Main/Main.tsx
+++ src/n1-main/m1-ui/routes/a0-Main/Main.tsx
@@ -1,18 +1,14 @@
-import { HashRouter, Navigate, Route, Routes } from "react-router-dom"
-import { Login } from "../../common/a1-Login/Login"
-import { Registration } from "../../common/a2-Registration/Registration"
-import { ForgotPassword } from "../../common/a3-ForgotPasword/ForgotPassword"
-import { NewPassword } from "../../common/a4-NewPassword/NewRassword"
-import { Profile } from "../../common/a5-Profile/Profile"
-import { TestComponent } from "../../common/a6-test/test"
-import { Nav } from "../a1-Navlink/Navlink"
-<<<<<<<<< Temporary merge branch 1
+
 import classes from "./Main.module.css"
-=========
-import React from "react";
-import {useSelector} from "react-redux";
-import {RootReducerType} from "../../../../n3-redux/a1-store/store";
->>>>>>>>> Temporary merge branch 2
+import {NewPassword} from "../../common/a4-NewPassword/NewRassword";
+import {Nav} from "../a1-Navlink/Navlink";
+import {TestComponent} from "../../common/a6-test/test";
+import {Registration} from "../../common/a2-Registration/Registration";
+import {Login} from "../../common/a1-Login/Login";
+import {ForgotPassword} from "../../common/a3-ForgotPasword/ForgotPassword";
+import {Profile} from "../../common/a5-Profile/Profile";
+import {Navigate, Route, Routes } from "react-router-dom";
+
 
 export enum pathEnum {
     main = '/',
